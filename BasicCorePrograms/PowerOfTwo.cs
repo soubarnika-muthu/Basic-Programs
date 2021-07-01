2fab7324ef76090950a4f34a4a34bfb13b8a1a4c
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-07-01 21:18:14 +0530

    [Soubarnika]Computing Power Of Two Program

---
+++ BasicCorePrograms/PowerOfTwo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+
+    class PowerOfTwo
+    {
+        ////Reading Input from the user
+        public static void ReadInput()
+        {
+
+            Console.WriteLine("Enter Power : ");
+            int Power = Convert.ToInt32(Console.ReadLine());
+            FindPowerOfTwo(Power);
+        }
+        //A Function to print Power Vlaues of two
+        public static void FindPowerOfTwo(int num)
+        {
+
+            for (int i = 0; i <= num; i++)
+            {
+                Console.WriteLine(Math.Pow(2, i) + " ");
+            }
+        }
+    }
+}
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -2,26 +2,37 @@
 
 namespace BasicCorePrograms
 {
+    // 1.Flip coin program
+    // 2.Leap Year Program
+    // 3.Power Of Two Program
+   
+
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Implementation of basic programs");
             Console.WriteLine("Enter choice to select programs: ");
-            Console.WriteLine("1.Flip Coin 2.Leap Year");
+            Console.WriteLine("1.Flip Coin 2.Leap Year 3. Power Of Two");
             int choice = Convert.ToInt32(Console.ReadLine());
-            //selection statement
+            //Selection statement
             switch (choice)
             {
                 case 1:
+                    //Computing Flip Coin Program
                     Console.WriteLine("Flip Coin Program!");
                     FlipCoin.ReadInput();
                     break;
                 case 2:
+                    //Computing Leap Year Program
                     Console.WriteLine("Finding Leap Year Or Not!");
                     LeapYear.ReadInput();
                     break;
-
+                case 3:
+                    //Computing Power Of Two Program
+                    Console.WriteLine("Finding Power Of Two");
+                    PowerOfTwo.ReadInput();
+                    break;
             }
 
         }
